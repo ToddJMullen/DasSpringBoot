5a5bb6b8b799e15234de1a39cca1439fa6db0ceb
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-01-14 00:15:01 -0500

    Mark Shipwreck.java as a JPA Entity & identify its PK field

--- src/main/java/com/boot/model/Shipwreck.java
+++ src/main/java/com/boot/model/Shipwreck.java
@@ -1,7 +1,17 @@
 package com.boot.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Shipwreck {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	Long id;
+//	@Column(name="NAME" ... other data ...)
+//	^^^ not needed because the columns match
 	String name;
 	String description;
 	String condition;
