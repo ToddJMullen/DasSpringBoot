d37de2082e0ac84a5ba9973c961e3e12490e0ce2
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-01-19 20:58:12 -0500

    Formatting only / preference changes

--- src/main/java/com/boot/model/Shipwreck.java
+++ src/main/java/com/boot/model/Shipwreck.java
@@ -23,14 +23,14 @@ public class Shipwreck {
 	public Shipwreck() { }
 
 	public Shipwreck(Long id, String name, String description, String condition, Integer depth, Double latitude, Double longitude, Integer yearDiscovered) {
-		this.id = id;
-		this.name = name;
-		this.description = description;
-		this.condition = condition;
-		this.depth = depth;
-		this.latitude = latitude;
-		this.longitude = longitude;
-		this.yearDiscovered = yearDiscovered;
+		this.id				= id;
+		this.name			= name;
+		this.description	= description;
+		this.condition		= condition;
+		this.depth			= depth;
+		this.latitude		= latitude;
+		this.longitude		= longitude;
+		this.yearDiscovered	= yearDiscovered;
 	}
 
 	public Long getId() {
